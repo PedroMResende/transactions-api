212593315df071cd19960041e6e1d5f9210f2902
Pedro Resende <professorpedroresende@gmail.com>
2026-09-08 17:31:52 -0300

    feat: add user roles to schema sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,11 @@
+CREATE TYPE user_role AS ENUM ('user', 'admin')
+
 CREATE TABLE users (
     id SERIAL PRIMARY KEY, 
     name VARCHAR(100) NOT NULL, 
     email VARCHAR(150) NOT NULL UNIQUE, 
     password VARCHAR(255) NOT NULL, 
+    role user_role NOT NULL DEFAULT 'user', 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
 
